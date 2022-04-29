8336a752b7d22a0833335b028b4decc338a15340
2nd-junkey <broken.phantsm@gmail.com>
2022-04-29 11:05:25 +0900

    一旦ざっと作ってみたので保存

--- Tools/convert.sh
+++ Tools/convert.sh
@@ -1,36 +1,69 @@
 #!/bin/bash
 
-# 引数を対象サークルのパスとして対応
-cd "$1"
-$SOURCEPATH="$PWD"
-echo "$SOURCEPATH"
+# 引数を"/root/**/サークル/作品"の親ディレクトリのパスとして処理
+if [ "$1" == "" ]; then
+    echo "第一引数(対象)未設定"
+    exit 1
+fi
+if [ ! -d "$1" ]; then
+    echo "directory($1) is not exist"
+    exit 1
+fi
+TARGETROOT="$1"
 
-# 一時避難先のパス
-OUTPUTPATH="$(mktemp -d)"
-echo "$OUTPUTPATH"
+if [ "$2" == "" ]; then
+    echo "第二引数(保存先)未設定"
+fi
+if [ ! -d "$2" ]; then
+    echo "directory($2) is not exit"
+fi
+SAVEROOT=""$2""
 
-# 掃い出し
-cd "$SOURCEPATH"
-mv -v * "$OUTPUTPATH/"
-
-# ベアリポジトリに変換
-git init --bare
-
-# リポジトリ登録
-cd "$OUTPUTPATH"
+cd "$TARGETROOT"
 git init
-# 登録前のあれこれ
 git config core.autocrlf false
-find . -type d -empty -exec touch "{}/.gitkeep" \;
-git add .
-# メッセージはﾃｷﾄｰ
-git commit -m init
-git remote add origin "$SOURCEPATH"
-git push --set-upstream origin master
 
-# 後処理
-cd "$SOURCEPATH"
-rm -vfr "$OUTPUTPATH"
+find "$TARGETROOT" -mindepth 3 -maxdepth 3 -type d -not -path "*/.git/*" | while read dname; do
+  #echo "dname: $dname"
+  cd "$dname"
+  # サークル
+  cd ..
+  CIRCLE=`basename "$(pwd)"`
+  #echo "circle: $CIRCLE"
+  cd - > /dev/null
 
+  # 作品
+  PRODUCT=`basename "$(pwd)"`
+  #echo "product: $PRODUCT"
+
+  # 保存先の更新
+  SAVEPATH="$SAVEROOT/$CIRCLE/$PRODUCT"
+  #echo "savepath: $SAVEPATH"
+
+
+  # 出力先の処理
+  mkdir -p "$SAVEPATH"
+  cd "$SAVEPATH"
+  git init --bare
+  cd -
+
+  # リポジトリ
+  git init
+  git config core.autocrlf false
+  find . -type d -empty -exec touch "{}/.gitkeep" \;
+  git add .
+  git commit -m init
+  git remote add origin "$SAVEPATH"
+  git push --set-upstream origin master
+
+  # submodule登録
+  cd ..
+  rm -fr "$PRODUCT"
+  git submodule add "$SAVEPATH"
+
+
+  # 事後処理
+  echo
+
+done
 exit $?
-
